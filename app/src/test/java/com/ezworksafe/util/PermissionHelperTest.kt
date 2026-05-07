88f53b5d63299c4ccefa48fad3d4dbe8cf781dca
Erik Zeek <zeekec@gmail.com>
2026-05-07 05:19:58 -0600

    fix: remove unused test rule and imports from PermissionHelperTest

--- app/src/test/java/com/ezworksafe/util/PermissionHelperTest.kt
+++ app/src/test/java/com/ezworksafe/util/PermissionHelperTest.kt
@@ -1,16 +1,10 @@
 package com.ezworksafe.util
 
 import android.Manifest
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Assert.assertArrayEquals
-import org.junit.Assert.assertEquals
-import org.junit.Rule
 import org.junit.Test
 
 class PermissionHelperTest {
-
-    @get:Rule
-    val instantRule = InstantTaskExecutorRule()
 
     @Test
     fun `required runtime permissions are CAMERA and RECORD_AUDIO`() {
