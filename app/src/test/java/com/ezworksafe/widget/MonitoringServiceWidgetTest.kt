c6caa9f8ff4593d9adae5f255b6dcc66267b4a24
Erik Zeek <zeekec@gmail.com>
2026-05-10 22:42:53 -0600

    test: add MonitoringServiceWidgetTest for widget state integration

---
+++ app/src/test/java/com/ezworksafe/widget/MonitoringServiceWidgetTest.kt
@@ -0,0 +1,38 @@
+package com.ezworksafe.widget
+
+import com.ezworksafe.data.model.SensorStatus
+import com.ezworksafe.data.model.SensorType
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class MonitoringServiceWidgetTest {
+
+    @Before
+    fun setUp() {
+        WidgetState.statuses = SensorType.entries.associateWith {
+            SensorStatus.Inactive
+        }
+    }
+
+    @Test
+    fun `widget state is updated with all four sensor statuses`() {
+        val statuses = mapOf(
+            SensorType.WIFI to SensorStatus.Active,
+            SensorType.BLUETOOTH to SensorStatus.Blocked,
+            SensorType.MICROPHONE to SensorStatus.Active,
+            SensorType.CAMERA to SensorStatus.Denied
+        )
+        WidgetState.statuses = statuses
+
+        val wifiLabel = WidgetState.statuses[SensorType.WIFI]?.label
+        val btLabel = WidgetState.statuses[SensorType.BLUETOOTH]?.label
+        val micLabel = WidgetState.statuses[SensorType.MICROPHONE]?.label
+        val camLabel = WidgetState.statuses[SensorType.CAMERA]?.label
+
+        assertEquals("Active", wifiLabel)
+        assertEquals("Blocked", btLabel)
+        assertEquals("Active", micLabel)
+        assertEquals("Denied", camLabel)
+    }
+}
