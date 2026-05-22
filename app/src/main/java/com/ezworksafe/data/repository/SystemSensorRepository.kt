7eebf29e1c3788a271a1c82f4601530d351cf37b
Erik Zeek <zeekec@gmail.com>
2026-05-22 07:17:45 -0600

    Suppress ExperimentalCoroutinesApi warning on flatMapLatest (#79)

--- app/src/main/java/com/ezworksafe/data/repository/SystemSensorRepository.kt
+++ app/src/main/java/com/ezworksafe/data/repository/SystemSensorRepository.kt
@@ -22,6 +22,7 @@ import android.util.Log
 import androidx.core.content.ContextCompat
 import com.ezworksafe.data.model.SensorStatus
 import com.ezworksafe.data.model.SensorType
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -36,6 +37,7 @@ class SystemSensorRepository(private val context: Context) : SensorRepository {
         refreshTrigger.value++
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun observeSensor(type: SensorType): Flow<SensorStatus> {
         return refreshTrigger.flatMapLatest {
             when (type) {
